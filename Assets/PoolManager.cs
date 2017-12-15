1eaca4c6e2ab6d03ccfedf1e3dcd22e39a55673c
Manindu Wijewickrama <manindu90@gmail.com>
2017-12-15 19:35:25 +0530

    first commit

---
+++ Assets/PoolManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour 
+{
+    Dictionary<int, List<GameObject>> poolDictionary = new Dictionary<int, List<GameObject>>();
+
+    static PoolManager _instance;
+
+    public static PoolManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<PoolManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public void CreatePool(GameObject prefab, int poolSize)
+    {
+        int poolKey = prefab.GetInstanceID();
+
+        if (!poolDictionary.ContainsKey(poolKey))
+        {
+            poolDictionary.Add(poolKey, new List<GameObject>());
+
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject newObject = Instantiate(prefab);
+                poolDictionary[poolKey].Add(newObject);
+            }
+        }
+    }
+
+    public GameObject GetObject(GameObject prefab)
+    {
+        int poolKey = prefab.GetInstanceID();
+
+        if (poolDictionary.ContainsKey(poolKey))
+        {
+            List<GameObject> pool = poolDictionary[poolKey];
+
+            for (int i = 0; i < pool.Count; i++)
+            {
+                if (!pool[i].gameObject.activeInHierarchy)
+                {
+                    return pool[i];
+                }
+            }
+            GameObject newObject = Instantiate(prefab);
+            poolDictionary[poolKey].Add(newObject);
+            return newObject;
+        }
+        return null;
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField] GameObject cubePrefab;
+    [SerializeField] int poolSize = 20;
+
+    void Start()
+    {
+        PoolManager.instance.CreatePool(cubePrefab, poolSize);
+        InvokeRepeating("SpawnCubes", 1f, 2f);
+    }
+
+    void SpawnCubes()
+    {
+        GameObject cubeObject = PoolManager.instance.GetObject(cubePrefab);
+        cubeObject.transform.position = Vector3.zero;
+        cubeObject.SetActive(true);
+    }
+}
